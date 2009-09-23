bbfb93729921ae3b2ea0e086585532f20d8fe71e
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-09-23 14:40:11 -0300

    Whitespace OCD

--- lib/sortable.rb
+++ lib/sortable.rb
@@ -1,10 +1,10 @@
 module Sortable
   VERSION = '0.0.1'
-  
+
   def self.included(base)
     base.extend Sortable::ClassMethods
   end
-  
+
   module ClassMethods
     def sortable(*sorters)
       define_method :"<=>", lambda { |other|
